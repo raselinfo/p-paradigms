82ba7fca77f08985216cdf0fedc8ea00605bdaa8
Rasel Hossain <rabinarahman@gmail.com>
2022-09-15 22:58:44 +0600

    Constructor Function

--- README.md
+++ README.md
@@ -96,6 +96,25 @@ function createCircle(radius) {
 }
 ```
 
+### Constructor Function
+
+```js
+// Constructor Function
+function Circle(radius) {
+  //   console.log(this);
+  this.radius = radius;
+  this.draw = () => {
+    console.log("Draw");
+  };
+}
+
+// Circle.call({}, 5);  it's only return an object if return this from the Constructor function
+// Circle.apply({}, 5); it's only return an object if return this from the Constructor function
+// Circle.bind({}, 5); it's only return an object if return this from the Constructor function
+const circle = new Circle(5); // new keyword make an empty object {}
+console.log(circle);
+```
+
 ## EVENT DRIVEN PROGRAMMING
 
 In computer programming, event-driven programming is a programming paradigm
--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-const circle = {
-  radius: 4,
-  draw() {
+// Constructor Function
+function Circle(radius) {
+//   console.log(this);
+  this.radius = radius;
+  this.draw = () => {
     console.log("Draw");
-  },
-};
-
-const circle2 = {
-  radius: 4,
-  draw() {
-    console.log("Draw");
-  },
-};
-// if the object has one or more object , it's call "Behavior"
-// Factory Function : The Factory Function is similar to constructor functions/class functions, but instead of using new to create an object, factory functions simply creates an object and returns it. Factory Functions are a very useful tool in JavaScript
-
-function createCircle(radius) {
-  return {
-    radius,
-    draw() {
-      console.log("Draw");
-    },
   };
 }
+
+// Circle.call({}, 5);  it's only return an object if return this from the Constructor function
+// Circle.apply({}, 5); it's only return an object if return this from the Constructor function
+// Circle.bind({}, 5); it's only return an object if return this from the Constructor function
+const circle = new Circle(5);  // new keyword make an empty object {}
+console.log(circle)
+
